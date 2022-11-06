eee3007109828126ddb9555a652a47387cd65b88
vikas <manojkotary@gmail.com>
2022-11-06 14:13:13 +0530

    Education done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Education from "./Components/Education";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 
@@ -8,7 +9,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home/>} />
-          <Route exact path="/education" element={<h1>education page</h1>} />
+          <Route exact path="/education" element={<Education/>} />
           <Route exact path="/projects" element={<h1>projects page</h1>} />
           <Route exact path="/contact_me" element={<h1>contact page</h1>} />
         </Routes>
--- src/data.js
+++ src/data.js
@@ -1,3 +1,3 @@
-export const backgroundColor = "#edf1fa"; 
+export const backgroundColor = "rgb(237, 249, 254)"; 
 export const headingColor = "#001c55";
 export const textColor = "#001C55";
